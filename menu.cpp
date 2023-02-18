d697d116427dc3abe99aac7147a960586a58e560
AhrendSilas <88252887+AhrendSilas@users.noreply.github.com>
2023-02-18 13:00:40 -0800

    adding scan options

--- menu.cpp
+++ menu.cpp
@@ -1,7 +1,21 @@
+#include "projectHead.cpp"
 
 void menu(void)
 {
+	char str[11];
+
 	printf("Welcome to Explore WSU\n");
-	printf("Please enter the date: ");
+	printf("Please enter the date (MM/DD/YYYY): ");
+
+	printf("Enter the name of the song you would like to delete: ");
+	fgets(str, sizeof(str), stdin);
+
+	for (int i = 0; i < strlen(str); i++)
+	{
+		if (str[i] == '\n')
+		{
+			str[i] = '\0';
+		}
+	}
 	printf("would you like to: (1)view food options in Pullman\n\t(2)view study spots on campus\n\t(3) view the upcoming sports schedule\n\t(4) view activities on campus\n\t(5) find gym locations and hours\n\t(6) access the campus bus schedule\n")
 }
