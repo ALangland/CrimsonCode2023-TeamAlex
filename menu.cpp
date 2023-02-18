e04e8f5cfbb89b493d10b1a3252b76b32f09be17
AhrendSilas <88252887+AhrendSilas@users.noreply.github.com>
2023-02-18 12:54:45 -0800

    menu

---
+++ menu.cpp
@@ -0,0 +1,7 @@
+
+void menu(void)
+{
+	printf("Welcome to Explore WSU\n");
+	printf("Please enter the date: ");
+	printf("would you like to: (1)view food options in Pullman\n\t(2)view study spots on campus\n\t(3) view the upcoming sports schedule\n\t(4) view activities on campus\n\t(5) find gym locations and hours\n\t(6) access the campus bus schedule\n")
+}
